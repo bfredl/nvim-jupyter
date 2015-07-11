77499eb06bf7be06e5c27ca3ea718c3eaac729b4
Răzvan-Cosmin Rădulescu <razvanc87@gmail.com>
2015-07-11 23:41:05 +0100

    trying to figure out how this encoding works

--- rplugin/python3/nvim_jupyter.py
+++ rplugin/python3/nvim_jupyter.py
@@ -6,8 +6,8 @@ class NVimJupyter:
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.encoding
     @neovim.command('JConnect', nargs='*', sync=True)
+    @neovim.encoding
     def jconnect_handler(self, args):
         self.vim.current.line = (
             'JConnect: {}'.format(args)
